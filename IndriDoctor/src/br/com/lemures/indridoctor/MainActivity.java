a46f2ce199331d5b42d3cb55df9cbcb11e8d6fc1
lemure <andre.farias@lemures.com.br>
2014-06-16 17:20:40 -0300

    Commit 16/06/2014 - André Farias

--- IndriDoctor/src/br/com/lemures/indridoctor/MainActivity.java
+++ IndriDoctor/src/br/com/lemures/indridoctor/MainActivity.java
@@ -1,62 +1,92 @@
 package br.com.lemures.indridoctor;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends ActionBarActivity {
 
+	final Context context = this;
+	private Button btEsqueciSenha;  
+	private ImageButton btPerfilMedico;  
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		btEsqueciSenha =  (Button) findViewById(R.id.bt_esqueci_senha);
+		btPerfilMedico = (ImageButton) findViewById(R.id.bt_perfil_medico);
+		
+		/**
+		 * 
+		 */
+		btPerfilMedico.setOnClickListener(new OnClickListener() {
+			 
+			  @Override
+			  public void onClick(View arg0) {
+	 
+				  Intent intent = new Intent(context, PerfilMedicoActivity.class);
+				  startActivity(intent);
+				  
+			  }
+			});
+		
+		/**
+		 * 
+		 */
+		btEsqueciSenha.setOnClickListener(new OnClickListener() {
+			 
+			  @Override
+			  public void onClick(View arg0) {
+	 
+				final Dialog dialog = new Dialog(context);
+				dialog.setContentView(R.layout.activity_esqueci_senha);
+				dialog.setTitle("Reenvio de Senha");
+	 
+				Button dialogButton = (Button) dialog.findViewById(R.id.bt_cancelar);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+				dialogButton.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						dialog.dismiss();
+					}
+				});
+	 
+				dialog.show();
+			  }
+			});
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.main2, menu);
 		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	   @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	        switch (item.getItemId()) {
+	        case android.R.id.home:
+	            NavUtils.navigateUpFromSameTask(this);
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	        }
+	    }
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
 
 }
