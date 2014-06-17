2c0b94f8a72605809099fdab2c47894411c0f81b
lemure <andre.farias@lemures.com.br>
2014-06-17 11:41:55 -0300

    Manutenção Evolutiva - 17/06/2014 - André Farias

---
+++ IndriDoctor/src/br/com/lemures/indridoctor/constants/Constantes.java
@@ -0,0 +1,13 @@
+package br.com.lemures.indridoctor.constants;
+
+public abstract class Constantes {
+
+	
+public final static String IP_SERVER = "http://192.168.154.1:8080";
+
+public final static String NOME = "nome";
+public final static String EMAIL = "email";
+public final static String LOGIN = "login";
+public final static String SENHA = "senha";
+	
+}
